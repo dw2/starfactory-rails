bff958b6fe62aa602f1ebaee01a145048ec0afae
Douglas Waltman II <doug@dougwaltman.com>
2014-08-04 12:52:30 -0700

    Updated readme. Set default time zone config.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Starfactory App
+
+A tool for scheduling local, personal, classes for building the Interwebs.
+
+---
+
+ * Make sure PostgreSQL is running
+ * If deploying, make sure timezone is set `heroku config:add TZ="America/Los_Angeles"`
+ * Setup the database `rake db:create`, `rake db:migrate`, etc.
+ * Run the server `rails s`
+
+---
+MIT License
--- config/application.rb
+++ config/application.rb
@@ -14,12 +14,10 @@ module Starfactory
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    Rails.configuration.quotes = YAML::load_file "#{Rails.root}/lib/quotes.yaml"
   end
 end
